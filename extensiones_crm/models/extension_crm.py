39df748b9a4dc249f130bccd7aafe61699f8edbc
FranciscoTasso <franciscotasso@hotmail.com>
2020-06-05 13:30:32 -0300

    Extensiones crm

---
+++ extensiones_crm/models/extension_crm.py
@@ -0,0 +1,8 @@
+from odoo import models, fields, api, _
+
+class crm_lead(models.Model):
+    _inherit = 'crm.lead'
+    _description=''
+
+    descripcion_oportunidad=fields.Text(string="Descripcion de la oportunidad")
+    usuario=fields.Many2one('res.users','Nombre del usuario actual', default=lambda self: self.env.uid, readonly=True,computed='_usuario')
